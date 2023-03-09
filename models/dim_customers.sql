2e785657d3a613c607b11ed1879913b32cbf0705
parthibansnowflake@gmail.com <parthibansnowflake@gmail.com>
2023-03-09 12:06:56 +0000

    error

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -1,30 +1,18 @@
+{{ 
+    config( 
+            materialized="table"
+    )
+}}
+
 with customers as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from dbt_db.dbt_schema.customers
-
+    select * from {{ref('stg_customers')}}
 ),
-
-orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from dbt_db.dbt_schema.orders
-
+ orders as (
+    select * from {{ref('stg_orders')}}
 ),
+ customer_orders as (
 
-customer_orders as (
-
-    select
-        customer_id,
+    select customer_id,
 
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
